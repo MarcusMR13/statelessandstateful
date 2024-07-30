37d5297f6ba327413c8e2d6d41d7c679cab13e81
Marcus Marques <marcusnopb@gmail.com>
2024-07-30 02:26:36 -0300

    alterando

--- lib/screens/Initial_screen.dart
+++ lib/screens/Initial_screen.dart
@@ -23,17 +23,17 @@ class _InitialscreenState extends State<Initialscreen> {
         opacity: opacity ? 1 : 0,
         duration: const Duration(microseconds: 700),
         child: ListView(
-          children: [
-            const Task(
+          children: const [
+             Task(
               "Jogar de Myers",
               "https://conteudo.imguol.com.br/c/entretenimento/e9/2018/10/18/michael-myers-cada-vez-mais-assustador-no-novo-halloween-1539891706130_v2_900x506.jpg",
               5,
             ),
-            const Task(
+             Task(
                 "Jogar de Artista",
                 "https://preview.redd.it/vqb3n4gcejn91.jpg?width=1200&format=pjpg&auto=webp&s=9a3af0be055a735557b3830d66d439c93e8e73fa",
                 4),
-            const Task(
+             Task(
               "Jogar de Palhaço",
               "https://img3.pillowfort.social/posts/66e93f1d1f40a7a3bd98_small.jpeg",
               5,
